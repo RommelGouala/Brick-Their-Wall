0b10c5fa001acd58990020de60fcac193431745d
Rommel Gouala <rommelgouala@MacBook-Pro.lan>
2022-05-01 12:26:37 -0500

    small change

--- imageNsound.js
+++ imageNsound.js
@@ -1,8 +1,8 @@
 // images
 const backImg = new Image()
-backImg.src = "/assets/images/img2.jpeg" // all right reserved to Rommel Gouala
+backImg.src = "./assets/images/img2.jpeg" // all right reserved to Rommel Gouala
 
 // Sounds
 
 const backGroundMusic =  new Audio()
-backGroundMusic.src = "/assets/sounds/Song_back.mp3" //all right reserved to Rommel Gouala
+backGroundMusic.src = "./assets/sounds/Song_back.mp3" //all right reserved to Rommel Gouala
